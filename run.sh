c22254264f680d28f6803f04532f582dd043c8ac
Anwar639 <52928119+Anwar639@users.noreply.github.com>
2024-03-31 19:40:33 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-5ece2f8f-2d5d-4f0b-b282-7b4a7c42be50/)
 
 
 echo "$curl_output" >> Update.tsx
